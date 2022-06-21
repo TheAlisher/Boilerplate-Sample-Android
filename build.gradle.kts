9ba7de5b2999bfd1622d2ccef8d551d7d2572d49
TheAlisher <allisher.abenov@gmail.com>
2022-06-21 08:02:49 +0600

    Gradle is updated to 7.4.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.application") version "7.2.0" apply false
-    id("com.android.library") version "7.2.0" apply false
-    kotlin("android") version "1.6.21" apply false
+    id("com.android.application") version "7.2.1" apply false
+    id("com.android.library") version "7.2.1" apply false
+    kotlin("android") version "1.7.0" apply false
 
     // Navigation Safe Args
     id("androidx.navigation.safeargs.kotlin") version "2.4.2" apply false
