3805b864ae8563ef2e26dc547540802ada2e6c49
TheAlisher <allisher.abenov@gmail.com>
2022-08-28 14:28:37 +0600

    Fragment is updated 1.5.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,7 +63,7 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.5.1")
 
     // Fragment
-    implementation("androidx.fragment:fragment-ktx:1.5.0")
+    implementation("androidx.fragment:fragment-ktx:1.5.2")
 
     // Lifecycle
     val lifecycleVersion = "2.5.0"
