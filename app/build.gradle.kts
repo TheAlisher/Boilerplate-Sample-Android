af709ea5848abaf90160d164f8c14a3a54808f43
TheAlisher <allisher.abenov@gmail.com>
2022-10-18 21:53:43 +0600

    updated Activity version to 1.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
 
     // Activity
-    implementation("androidx.activity:activity-ktx:1.5.1")
+    implementation("androidx.activity:activity-ktx:1.6.0")
 
     // Fragment
     implementation("androidx.fragment:fragment-ktx:1.5.2")
