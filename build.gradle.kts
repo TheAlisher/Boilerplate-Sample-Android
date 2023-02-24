f267dbcab5249f41add0eff765adeee7d143e8ec
TheAlisher <allisher.abenov@gmail.com>
2023-02-24 11:14:24 +0600

    updated Kotlin version to 1.8.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application") version "7.4.1" apply false
     id("com.android.library") version "7.4.1" apply false
-    kotlin("android") version "1.7.20" apply false
+    kotlin("android") version "1.8.10" apply false
 
     // Navigation Safe Args
     id("androidx.navigation.safeargs.kotlin") version "2.5.2" apply false
